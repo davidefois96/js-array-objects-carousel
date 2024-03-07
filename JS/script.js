2b755a2f4ec52643e1afd3f1528bb3cf0a5e3a06
davidefois96 <davidefois360@gmail.com>
2024-03-07 09:31:45 +0100

    [add] new code

--- JS/script.js
+++ JS/script.js
@@ -72,26 +72,27 @@ const images = [
 
   }, 
   { 
-  url :'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg' ,
-  nome : 'Finlandia',
+  url :'https://www.sardegna.info/wp-content/uploads/sites/5/portoscuso.jpg' ,
+  nome : 'Lungomare Cristoforo Colombo',
   descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
 
   }, 
   { 
-  url :'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg' ,
-  nome : 'Norvegia',
+    
+  url :'https://www.radiox.it/wp-content/uploads/tonnaraportoscuso.jpg' ,
+  nome : 'Tonnara',
   descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
 
   }, 
   { 
-  url :'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg' ,
-  nome : 'Danimarca',
+  url :'https://www.sacruxitta.it/wp-content/uploads/2012/07/pano-da-scalette.jpg',
+  nome : 'Portopaglietto',
   descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
 
   }, 
   { 
-  url :'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg' ,
-  nome : 'Islanda',
+  url :'https://i.ytimg.com/vi/HADEzZGUz0c/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGCUgYShlMA8=&amp;rs=AOn4CLBmGaElfP-VSCUtrxQ2bCOP_mh6_g',
+  nome: 'Capoaltano',
   descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
 
   }, 
